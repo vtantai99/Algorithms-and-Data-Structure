0d2025a75989694c4191a0de0f8f7f80acc39c40
Tai Vo <tantaivo74@gmail.com>
2023-11-01 16:43:30 +0700

    problem solving

---
+++ Problem-Solving/doc.md
@@ -0,0 +1,7 @@
+# **Problem solving**
+
+1. Understand the Problem
+2. Explore Concrete Example
+3. Break It Down
+4. Solve/Simplify
+5. Look Back and Refactor
---
+++ Problem-Solving/index.ts
@@ -0,0 +1,17 @@
+// Write a function which takes in a string and returns counts of each character in the string.
+
+function charCount(str: string) {
+  // do something
+  // return an object with keys that are lowercase alphanumeric characters in the string; 
+  // values should be the counts for those characters
+}
+
+function charCountV2(str: string) {
+  // make object to return at end ...
+  // loop over string, for each character...
+  // 1. If the char is a number/letter AND is a key in object, add one to count
+  // 2. If the char is a number/letter AND not in object, add it to object and set count to 1
+  // 3. If the char is something else (space, period, etc.) don't do anything
+
+  // return object at end
+}
