e971be1d7299025acc8f7f01297f5ee0827c281e
Tai Vo <tantaivo74@gmail.com>
2023-11-05 14:10:00 +0700

    problem-solving: mutiple-pointers

--- Problem-Solving/doc.md
+++ Problem-Solving/doc.md
@@ -9,3 +9,6 @@
 ### Frequency counter pattern
 1. _This pattern uses objects or sets to collect values/frequencies of values_
 2. _This can often avoid the need for nested loops or O(N^2) operations with arrays/strings_
+### Multiple pointers pattern
+1. _Creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition_
+2. _Very efficient for solving problems with minimal space complexity as well_
---
+++ Problem-Solving/mutiple-pointers.ts
@@ -0,0 +1,42 @@
+// Write a function called 'sumZero' which accepts a sorted array of integers.
+// The function should find the 'first pair' where the sum is 0.
+// Return an array that includes both values that sum to zero or empty array if a pair does not exist.
+const sumZero = (array: number[]) => {
+  array.sort((a, b) => a - b);
+
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    const sum = array[left] + array[right];
+    if (sum === 0) {
+      return [array[left], array[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return [];
+};
+
+// const result = sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 5]);
+// console.log("David Vo 🚀 ~> result:", result);
+
+
+// Implement a function called 'countUniqueValues', which accepts a sorted array, and count the unique values in the array
+// There can be negative numbers in the array, but it will always be sorted
+const countUniqueValues = (array: number[]) => {
+  if (array.length === 0) return 0
+
+  let i = 0;
+  for (let j = 1; j < array.length; j++) {
+    if (array[i] !== array[j]) {
+      i++;
+      array[i] = array[j];
+    }
+  }
+  return i + 1
+}
+
+console.log(countUniqueValues([1, 1, 2, 2, 3]))
